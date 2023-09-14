e27b4962773e0067e9c37681fe37a099cc3b1e18
SoftwareX Plus <119097812+softwarexplus@users.noreply.github.com>
2023-09-14 12:56:04 +0600

    Update message.ts

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,60 +1,59 @@
+import { Client, ChannelType, Message } from "discord.js"
 import axios from "axios"
+import Payload from "./../payload"
 
-export default function log(client:any, webhook:string) {
-    client.on("messageCreate", (message:any) => {
-        logMessage(message, "Message Status: send")
+export default function log(client: Client, webhook: string) {
+    client.on("messageCreate", (message: Message) => {
+        logMessage(message, "Status: Create")
     })
 
-    // Log message edit
-    client.on("messageUpdate", (oldMessage:any, newMessage:any) => {
-        logMessage(newMessage, "Message Status: edit")
+    client.on("messageUpdate", (oldMessage: Message, newMessage: Message) => {
+        logMessage(newMessage, "Status: Edit")
     })
 
-    // Log message delete
-    client.on("messageDelete", (message:any) => {
-        logMessage(message, "Message Status: delete")
+    client.on("messageDelete", (message: Message) => {
+        logMessage(message, "Status: Delete")
     })
 
-    function logMessage(message:any, messageStatus:string) {
+    function logMessage(message: Message, messageStatus: string) {
+        if (message.channel.type === ChannelType.DM || message.embeds?.length || !message.content) return
+
         try {
-            if (!message.content) return
-            const embed = {
-                title: `Message from ${message.author.username}`,
-                description: message.content,
-                color: `5865F2`,
-                fields: [
+            const Payload: Payload = {
+                embeds: [
                     {
-                        name: `Server`,
-                        value: `${message.guild.name} | message.guild.id`,
-                    },
-                    {
-                        name: `Channel`,
-                        value: `${message.channel.name} | ${message.channel.id}`,
-                    },
-                    {
-                        name: `Author`,
-                        value: `${message.author.username} | ${message.author.id}`,
-                    },
-                    {
-                        name: `Message`,
-                        value: `${message.id} | ${messageStatus}`,
-                    },
-                ],
+                        title: `Message from ${message.author.username}`,
+                        description: message.content,
+                        color: 0x5865f2,
+                        fields: [
+                            {
+                                name: `Server`,
+                                value: `${message.guild.name} | ${message.guild.id}`
+                            },
+                            {
+                                name: `Channel`,
+                                value: `${message.channel.name} | ${message.channel.id}`
+                            },
+                            {
+                                name: `Author`,
+                                value: `${message.author.username} | ${message.author.id}`
+                            },
+                            {
+                                name: `Message`,
+                                value: `${message.id} | ${messageStatus}`
+                            }
+                        ]
+                    }
+                ]
             }
 
             try {
-                axios.post(webhook, {
-                    embeds: [embed],
-                })
+                axios.post(webhook, Payload)
             } catch (error: unknown) {
-                console.error(
-                    `[log-djs] FF03: Unable to send webhook \n${error}`
-                )
+                console.error(`[log-djs] FF03: Unable to send webhook \n${error}`)
             }
         } catch (error: unknown) {
-            console.error(
-                `[log-djs] FF04: Unable to create webhook messages \n${error}`
-            )
+            console.error(`[log-djs] FF04: Unable to create webhook messages \n${error}`)
         }
     }
 }
