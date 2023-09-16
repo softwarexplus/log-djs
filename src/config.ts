7e113fa1aeeeaa06293f8d274d45ee4e76cc606d
SoftwareX Plus <119097812+softwarexplus@users.noreply.github.com>
2023-09-16 10:58:58 +0600

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,29 +2,31 @@ import message from "./utils/message.js"
 import { Client } from "discord.js"
 import { version } from "./../package.json"
 import { get } from "axios"
+
 type option = {
     client: Client
     webhook: string
+    ignore?: Array<string>
     message?: boolean
 }
 
 export function config(object: option) {
     get("https://api.github.com/repos/softwarexplus/log-djs/releases/latest")
         .then((res) => {
-            if (res.data.tag_name !== version) {
-                console.log(
-                    `Your bot is not up to date! Please update to the latest version!`,
-                    version + " -> " + res.data.tag_name
-                )
+            if (!res.data.tag_name.includes("beta") && !res.data.tag_name.includes("alpha")) {
+                if (res.data.tag_name !== version) {
+                    console.log(
+                        `Your package is not up to date! Please update to the latest version!`,
+                        version + " -> " + res.data.tag_name
+                    )
+                }
             }
         })
-        .catch((err) => {
-            console.log(`Failed to check if bot is up to date!`)
-        })
+        .catch((err) => console.log(`Failed to check if package is up to date!`))
 
     if (!object.client) throw `[log-djs] FF01: client missing`
     if (!object.webhook) throw `[log-djs] FF02: webhook missing`
     if (object.message) {
-        message(object.client, object.webhook)
+        message(object.client, object.webhook, object.ignore)
     }
 }
